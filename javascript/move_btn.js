3ce21f0e6c6af014152b3441927f036e4e4e95c0
Desm0nt <logran2@gmail.com>
2024-05-17 10:13:14 +0300

    Add files via upload

--- javascript/move_btn.js
+++ javascript/move_btn.js
@@ -1,6 +1,6 @@
 onUiLoaded(async () => {
     const container = document.getElementById('quicksettings');
-    const btn = document.getElementById('cls_btn');
+    const btn = document.getElementById('cls_btn2');
     btn.classList.add('tool');
     container.appendChild(btn);
 });
